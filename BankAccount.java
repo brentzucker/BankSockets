b22c9363858cc9304ddf13824646a224e863c22e
brentzucker <brentzucker@gmail.com>
2015-09-17 23:40:07 -0400

    BankAccount: username, password, balance
    - Might need a transaction counter

---
+++ BankAccount.java
@@ -0,0 +1,38 @@
+public class BankAccount {
+
+	private String username;
+	private String password;
+	private Double balance;
+
+	// Transaction counter?
+	
+	public BankAccount(String username, String password, String balance) {
+		this.username = username;
+		this.password = password;
+		this.balance = balance;
+	}
+
+	public String getUsername() {
+	    return this.username;
+	}
+
+	public String getPassword() {
+	    return this.password;
+	}
+
+	public Double getBalance() {
+	    return this.balance;
+	}
+
+	public void setUsername(String username) {
+	    this.username = username;
+	}
+
+	public void setPassword(String password) {
+	    this.password = password;
+	}
+
+	public void setBalance(Double balance) {
+	    this.balance = balance;
+	}
+}
